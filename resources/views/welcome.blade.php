e0ecc6dfb5871721a8b4dfe8caa00062698f2404
yuuya210618 <yuuya210618@gmail.com>
2023-08-12 20:37:10 +0900

    コントローラー作成

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,8 @@
     <title>Todo</title>
 </head>
 <body>
-    <form method="post" action="">
+<form method="post" action="{{ route('store') }}"> <!-- ここを編集 -->
+        @csrf 
         <input type="text" name="text">
         <button>データ追加</button>
     </form>
